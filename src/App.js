8debbcc8bd57f1ee2fd0aae2351d32be7cf1d845
Madcodes831 <r.medina2119@gmail.com>
2024-05-11 18:05:57 -0700

    restructure project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LoginForm } from "./form/LoginForm";
-import { RegisterForm } from "./form/RegisterForm";
+
 import { HomePage } from "./home/HomePage";
 import { NavBar } from "./nav/NavBar";
-import { SignIn } from "./sign-in/SignIn";
+
 
 
 function App() {
@@ -15,9 +14,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route exact path="/" element={<HomePage />} />
-          <Route exact path="/login" element={<LoginForm />} />
-          <Route exact path="/register" element={<RegisterForm />} />
-          <Route exact path="/signin" element={<SignIn/>}/>
+  
         </Routes>
       </BrowserRouter>
     </div>
