4bf0810ffd286084c676d61b498f8c96cae73d2a
Ricardo Medina <r.medina2119@gmail.com>
2024-09-08 15:12:18 -0700

    Ui

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -3,7 +3,7 @@ import React from "react";
 export const HomePage = () => {
   return (
     <div>
-      <h1>Best videos, funny videos </h1>
+      <h1>Best videos on the internet </h1>
       <h2> Search</h2>
       <input type="text"></input>
     <button>Sign up</button>
