13a40e6196600fef0020782562a2d18b1f91b27b
Ricardo Medina <r.medina2119@gmail.com>
2024-09-22 12:49:51 -0700

    more content

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -18,6 +18,7 @@ export const HomePage = () => {
       <h2> I need a better plan</h2>
       <h2> Brainstorm</h2>
       <h3> I need a new plan </h3>
+      <h3> Im sad</h3>
       <h2> I need help</h2>
     </div>
   );
