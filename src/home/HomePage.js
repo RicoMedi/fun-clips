3e0223294d2b5d3c8833f690b6811429a215b327
Ricardo Medina <r.medina2119@gmail.com>
2024-10-13 21:50:13 -0700

    clean up

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -4,11 +4,7 @@ export const HomePage = () => {
   return (
     <div>
       <h1> Travel Videos</h1>
-      <h1> Best Travel videos </h1>
-      <h2> Search</h2>
-      <input type="text"></input>
-    <button>Sign up</button>
-    
+   
     
 
   
