1ab71cf893b6720299b0917b161034da84c5a3ea
Ricardo Medina <r.medina2119@gmail.com>
2024-11-10 21:04:09 -0800

    Upgrade packages

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -19,16 +19,7 @@ export const HomePage = () => {
       <h1>Home Page</h1>
       <p>Welcome to the Home Page</p>
      </section>
-     <section>
-      <h2>Features</h2>
-      <ul>
-        <li>Feature 1</li>
-        <li>Feature 2</li>
-        <li>Feature 3</li>
-      </ul>
-     </section>
-    \
-   
+
     
     
 
